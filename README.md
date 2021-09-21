cf693adac54d4edca515cc85fb642d091e523513
pavlo-osypov-by <pavlo.osypov@edvantis.com>
2021-09-21 17:23:55 +0300

    Heroku & Netlify

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Chat App created using [getstream.io](https://getstream.io/)
 
 ## Full fledged live chat application using React on the front, NodeJS on the back
+
+[Site](https://streamio-chat.netlify.app/)
+
+[Heroku](https://streamio-chat.herokuapp.com/)
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -28,8 +28,8 @@ const Auth = () => {
 
     const { username, password, phoneNumber, avatarURL } = form;
 
-    const URL = 'http://localhost:5000/auth';
-    // const URL = 'https://medical-pager.herokuapp.com/auth';
+    // const URL = 'http://localhost:5000/auth';
+    const URL = 'https://streamio-chat.herokuapp.com/';
 
     const { data: { token, userId, hashedPassword, fullName } }
       = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
