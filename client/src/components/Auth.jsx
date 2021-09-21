872e95236f91d9482d2929fa973493acab4d9968
pavlo-osypov-by <pavlo.osypov@edvantis.com>
2021-09-21 18:22:54 +0300

    Auth.js

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -29,7 +29,7 @@ const Auth = () => {
     const { username, password, phoneNumber, avatarURL } = form;
 
     // const URL = 'http://localhost:5000/auth';
-    const URL = 'https://streamio-chat.herokuapp.com/';
+    const URL = 'https://streamio-chat.herokuapp.com/auth'
 
     const { data: { token, userId, hashedPassword, fullName } }
       = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
